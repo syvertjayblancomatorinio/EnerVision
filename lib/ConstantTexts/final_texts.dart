fffdcdee67371a6ea2f1158af39f1c092d5264d5
Syvert Jay Martorinio <syvert@geidi.com>
2024-11-08 01:52:53 +0800

    no changes in code, Just cleaning up.

--- lib/ConstantTexts/final_texts.dart
+++ lib/ConstantTexts/final_texts.dart
@@ -1,5 +1,3 @@
-const supabaseUrl = 'https://umkzrspyprfwsruragzj.supabase.co';
-
 const supabaseKey = String.fromEnvironment(
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVta3pyc3B5cHJmd3NydXJhZ3pqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwNzg4MTYsImV4cCI6MjAzOTY1NDgxNn0.xiTz7LTNn6mT3zFcSr078P_kO5vjo9uXUOAVBNvCaQQ');
 const String appname = 'EverVision';
@@ -7,12 +5,9 @@ const String renewableEnergyP1 =
     'Renewable energy  comes from natural sources that are constantly replenished. These sources are crucial for a sustainable future.';
 const String renewableEnergyP2 =
     'Renewable energy  can be used for electricity generation, space and water heating and cooling, and transportation. Non-renewable energy, in contrast, comes from finite sources, such as coal, natural gas, and oil.';
-
 const String energyTrackerExplainer =
     'Personalized tool for efficient energy management. Seamlessly calculate your average energy costs by inputting watts, appliance usage, and prevailing kWh rates.';
 
-String daysLabel = 'Days';
-String weekLabel = 'Week';
 const List<String> days = <String>[
   'One',
   'Two',
